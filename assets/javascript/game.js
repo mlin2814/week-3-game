3d073f22f8bf20cd9a10d4df32e3c6da530f309c
Matthew Lin <Matthew Lin>
2016-08-17 12:06:01 -0400

    Added in input from TA and adjusted to fill in for approrpriate sections

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,24 @@
-<script type="text/javascript">
-	var alphabet = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
-	var hello = alert(Would you like to play?);
-	var wins = 0;
-	var wordBank = ["Steven", "Rose", "Greg", "Pearl", "Garnet", "Amethyst", "Sapphire", "Ruby", "Peridot", "Lapis", "Jasper", "Sadie", "Lars", "Onion"];
-	var lines = [_______________];
-	var chances = 10;
+
+	// var alphabet = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
+	// var hello = alert("Would you like to play?");
+	// var wins = 0;
+	var wordBank = ["Steven", "Rose", "Greg","Pearl", "Garnet", "Amethyst", "Sapphire", "Ruby", "Peridot", "Lapis", "Jasper", "Sadie", "Lars", "Onion"];
+	// var chances = 10;
 
 document.onkeyup = function(event) {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
-	var computerGuess = wordBank[Math.floor(Math.random() * wordBank.length)];
-
-if (userGuess == computerGuess) {
-	continue++;
-}else if (userGuess != computerGuess) {
-	losses;
+console.log(userGuess);
 }
 
-if (lines == wordBank) {
-	continue++;
-}
+// 	var computerGuess = wordBank[Math.floor(Math.random() * wordBank.length)];
 
-guessButton = document.getElementById("guess");
-        guessInput.style.display = 'inline';
-        guessButton.style.display = 'inline';
+// if (userGuess == computerGuess) {
+// 	continue++;
+// }else if (userGuess != computerGuess) {
+// 	losses;
+// }
 
-
-
-
-
-</script>
+// guessButton = document.getElementById("guess");
+//         guessInput.style.display = 'inline';
+//         guessButton.style.display = 'inline';
+ 
